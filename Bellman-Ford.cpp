3ee1c4bd5b27ac23dd946a2bb66d0e2d7ff28a64
Shreeja Princy <148578262+ShreejaPrincy@users.noreply.github.com>
2026-07-14 01:00:48 +0530

    Create Bellman-Ford.cpp

---
+++ Bellman-Ford.cpp
@@ -0,0 +1,45 @@
+// Time Complexity:
+// O(V * E)
+// V = Number of vertices
+// E = Number of edges
+//
+// We relax all E edges exactly (V - 1) times,
+// followed by one extra iteration to detect a negative weight cycle.
+
+// Space Complexity:
+// O(V)
+// O(V) for the distance array.
+
+class Solution {
+	public:
+	vector<int> bellmanFord(int V, vector<vector<int>> & edges, int src) {
+		
+		vector<int>dist(V, 1e8);
+		dist[src]=0;
+		
+		for (int i = 0; i<V - 1; i++) {
+			for (auto it:edges) {
+				int u = it[0];
+				int v = it[1];
+				int w = it[2];
+				
+				if (dist[u] != 1e8 && dist[u]+w<dist[v]) {
+					dist[v] = dist[u]+w;
+				}
+			}
+		}
+		
+		// nth iteration
+		for (auto it:edges) {
+			int u = it[0];
+			int v = it[1];
+			int w = it[2];
+			
+			if (dist[u] != 1e8 && dist[u]+w<dist[v]) {
+				return {-1};
+			}
+		}
+	
+	return dist;
+}
+};
